5f2e757987e0a9f671ba69d1ce4043ea044c67a7
georg <georbearwolf.com>
2023-03-01 16:29:55 +0300

    [012] Step 6, Task 2

---
+++ brain_games/scripts/games/brain_calc.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python3
+
+from pprint import pprint
+
+import sys
+print(sys.path)
+
+
+print('123')
+print(greeting_welcome.name)
---
+++ brain_games/scripts/greeting_welcome.py
@@ -0,0 +1,6 @@
+import prompt
+
+
+print('Welcome to the Brain Games!')
+name = prompt.string('May I have your name? ')
+print(f'{"Hello, " + name + "!"}')
