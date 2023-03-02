507e337388c8bdf01efe627753ee124d4e13de78
georg <georbearwolf.com>
2023-03-02 16:30:17 +0300

    [013] Step 6, Task 2

--- brain_games/scripts/games/brain_calc.py
+++ brain_games/scripts/games/brain_calc.py
@@ -1,10 +1,16 @@
 # !/usr/bin/env python3
 
 from pprint import pprint
-
 import sys
 print(sys.path)
 
 
-print('123')
-print(greeting_welcome.name)
+# from .. import greeting_welcome
+
+# import sys
+# sys.path.append(r'/python-project-49/brain_games/scripts')
+# import scripts.greeting_welcome.py
+# import pprint
+
+# import greeting_welcome
+# scripts.greeting_welcome.qwerty()
--- brain_games/scripts/greeting_welcome.py
+++ brain_games/scripts/greeting_welcome.py
@@ -4,3 +4,6 @@ import prompt
 print('Welcome to the Brain Games!')
 name = prompt.string('May I have your name? ')
 print(f'{"Hello, " + name + "!"}')
+
+def qwerty():
+    print('1234567')
