c1ac3638d3a701f83ea25579ca59739784337c64
georg <georbearwolf.com>
2023-03-05 19:49:11 +0300

    [016] Step 5, Task 2 fixed random number logic

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 [![Actions Status](https://github.com/zitaker/python-project-49/workflows/hexlet-check/badge.svg)](https://github.com/zitaker/python-project-49/actions)
 
 Welcome to the Brain Games!
-brain-even https://asciinema.org/a/6HJt3Ie4gzkVnm7Mftk4E3IfS
+brain-even https://asciinema.org/a/cIVN9YvpfjNBsFtJcEykriZhk
 
--- brain_games/scripts/games/brain_even.py
+++ brain_games/scripts/games/brain_even.py
@@ -3,7 +3,8 @@ import prompt
 from random import randint
 
 
-def qwerty():
+def welcome_user_even():
+    print('brain-even')
     print('Welcome to the Brain Games!')
     names = prompt.string('May I have your name? ')
     print(f'{"Hello, " + names + "!"}')
@@ -13,40 +14,38 @@ def qwerty():
     return names
 
 
-name = qwerty()
+name = welcome_user_even()
 
 
 def life_user():
     # life - кол-во жизней
-    life = 1
+    life = 3
     while life > 0:
         #   randint - задать random число в промежутке
-        question_numbers = [randint(0, 10)]
+        question_numbers = randint(0, 10)
         #   correct_response - считать ответы
         correct_response = 0
-        # numbers равен элементу в списке question_numbers
-        for numbers in question_numbers:
-            print('Question:', numbers)
+        print('Question:', question_numbers)
 
-            if numbers % 2 == 0:
-                if input('Your answer: ') == 'yes':
-                    print('Correct!')
-                    correct_response = correct_response + 1
-                else:
-                    print("'yes' is wrong answer ;(."
-                          " Correct answer was 'no'. "
-                          "Let's try again, " + name + '!')
-                    correct_response = correct_response - 1
-
+        if question_numbers % 2 == 0:
+            if input('Your answer: ') == 'yes':
+                print('Correct!')
+                correct_response = correct_response + 1
             else:
-                if input('Your answer: ') == 'no':
-                    print('Correct!')
-                    correct_response = correct_response + 1
-                else:
-                    print("'yes' is wrong answer ;(."
-                          " Correct answer was 'no'. "
-                          "Let's try again, " + name + '!')
-                    correct_response = correct_response - 1
+                print("'yes' is wrong answer ;(."
+                      " Correct answer was 'no'. "
+                      "Let's try again, " + name + '!')
+                correct_response = correct_response - 1
+
+        else:
+            if input('Your answer: ') == 'no':
+                print('Correct!')
+                correct_response = correct_response + 1
+            else:
+                print("'yes' is wrong answer ;(."
+                      " Correct answer was 'no'. "
+                      "Let's try again, " + name + '!')
+                correct_response = correct_response - 1
 
         if correct_response < 0:
             break
