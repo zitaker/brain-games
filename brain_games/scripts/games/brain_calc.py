700e15870c6dd512beb26fdcc31e5b9da590bccd
georg <georbearwolf.com>
2023-03-06 16:21:37 +0300

    [018] Step 6, number of levels

--- brain_games/scripts/games/brain_calc.py
+++ brain_games/scripts/games/brain_calc.py
@@ -1,25 +1,52 @@
 # !/usr/bin/env python3
+import operator
 import prompt
-from random import randint, randrange, getrandbits, choice
+from random import randint
 
 
-# def welcome_user_calc():
-#     print('brain-calc')
-#     print('Welcome to the Brain Games!')
-#     names = prompt.string('May I have your name? ')
-#     print(f'{"Hello, " + names + "!"}')
-#
-#     print('What is the result of the expression?')
-#     return names
-#
-#
-# name = welcome_user_calc()
-a = 2
-b = '*'
-d = {a : 1, 'dictionary': 2, int(22): 3, b: 4}
-print(choice(list(d)))
+def welcome_user_calc():
+    print('brain-calc')
+    print('Welcome to the Brain Games!')
+    names = prompt.string('May I have your name? ')
+    print(f'{"Hello, " + names + "!"}')
+
+    print('What is the result of the expression?')
+    return names
 
 
+name = welcome_user_calc()
+
+def types_operation():
+    number_1 = randint(0, 10)
+    number_2 = randint(0, 10)
+
+    result = randint(0, 2)
+
+    if result == 0:
+        result_expression = (number_1 + number_2)
+        operations = '+'
+    elif result == 1:
+        result_expression = (number_1 - number_2)
+        operations = '-'
+    else:
+        result_expression = (number_1 * number_2)
+        operations = '*'
+
+    print('Question:', number_1, operations, number_2)
+    return result_expression
 
 
+result_expressions = types_operation()
 
+def checking_answers():
+    answer = int(input('Your answer: '))
+
+    if answer == result_expressions:
+        print('Correct!')
+    else:
+        print(f"'{answer}'", "is wrong answer ;(. "
+                             "Correct answer was", f"'{result_expressions}'" + ". "
+                             "Let's try again,", name + "!")
+
+
+checking_answers()
