63b5bf342c622e23419aeb49873581fcb4ec3301
georg <georbearwolf.com>
2023-03-06 06:17:15 +0300

    [017] Step 6 logic of operations

--- brain_games/scripts/games/brain_calc.py
+++ brain_games/scripts/games/brain_calc.py
@@ -1,22 +1,25 @@
 # !/usr/bin/env python3
 import prompt
+from random import randint, randrange, getrandbits, choice
 
 
-print('brain-calc')
-def welcome_user():
-    print('Welcome to the Brain Games!')
-    names = prompt.string('May I have your name? ')
-    print(f'{"Hello, " + names + "!"}')
-    return names
+# def welcome_user_calc():
+#     print('brain-calc')
+#     print('Welcome to the Brain Games!')
+#     names = prompt.string('May I have your name? ')
+#     print(f'{"Hello, " + names + "!"}')
+#
+#     print('What is the result of the expression?')
+#     return names
+#
+#
+# name = welcome_user_calc()
+a = 2
+b = '*'
+d = {a : 1, 'dictionary': 2, int(22): 3, b: 4}
+print(choice(list(d)))
 
-# name = welcome_user()
 
-print('What is the result of the expression?')
 
-calculations = [4 + 10, 5 + 2]
-
-print('Question: 4 + 10')
-calculation_calculator = input('Your answer: ')
-print(calculations[1])
 
 
