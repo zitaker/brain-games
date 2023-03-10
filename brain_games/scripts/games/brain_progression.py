89e2091696c882ff04d9aed85a88a3ac07602010
georg <georbearwolf.com>
2023-03-10 14:32:53 +0300

    [025] Step 8, Traceback

--- brain_games/scripts/games/brain_progression.py
+++ brain_games/scripts/games/brain_progression.py
@@ -13,22 +13,7 @@ from random import randint
 #
 #
 # name = welcome_user_progression()
-name = 111111
-
-
-def arithmetic_progression():
-    progression_1 = randint(1, 10)
-    progression_2 = randint(1, 10)
-    progression_3 = randint(100, 200)
-    index = (list(range(progression_2, progression_3, progression_1)))
-    return index
-
-# n = 0
-# x = 5
-# while n < x:
-#     i = arithmetic_progression()
-
-i = arithmetic_progression()
+name = 'qwerty123'
 
 
 def result_progression():
@@ -37,14 +22,25 @@ def result_progression():
     while life > 0:
         life = life - 1
 
+        def arithmetic_progression():
+            progression_1 = randint(1, 10)
+            progression_2 = randint(1, 10)
+            progression_3 = randint(100, 200)
+            index = (list(range(progression_2, progression_3, progression_1)))
+            return index
+
+        i = arithmetic_progression()
+
         min_progression = 5
         max_progression = randint(0, 10)
         progres = (i[0:min_progression] + i[min_progression:max_progression])
 
         num_input = randint(0, max_progression)
+        print(progres[num_input])
 
         progres_str = (" ".join(map(str, progres)))
         num_replace = str(progres_str).replace(str(progres[num_input]), '..')
+        #  num_replace = str(progres_str).replace(str(progres[num_input]), '..')
         print(num_replace)
 
         comparison_num_input = int(input('Your answer: '))
