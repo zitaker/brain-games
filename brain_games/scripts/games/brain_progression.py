f3f1eb2dd117a3eb765a6a431d78539f61d84e40
georg <georbearwolf.com>
2023-03-10 13:32:19 +0300

    [024] Step 8, error repeating arithmetic_progression()

--- brain_games/scripts/games/brain_progression.py
+++ brain_games/scripts/games/brain_progression.py
@@ -1,5 +1,5 @@
 # !/usr/bin/env python3
-# import prompt
+import prompt
 from random import randint
 
 
@@ -13,6 +13,9 @@ from random import randint
 #
 #
 # name = welcome_user_progression()
+name = 111111
+
+
 def arithmetic_progression():
     progression_1 = randint(1, 10)
     progression_2 = randint(1, 10)
@@ -20,22 +23,43 @@ def arithmetic_progression():
     index = (list(range(progression_2, progression_3, progression_1)))
     return index
 
+# n = 0
+# x = 5
+# while n < x:
+#     i = arithmetic_progression()
 
 i = arithmetic_progression()
 
 
 def result_progression():
-    min_progression = 5
-    max_progression = randint(0, 10)
-    progression = (i[0:min_progression] + i[min_progression:max_progression])
+    # life - кол-во жизней
+    life = 3
+    while life > 0:
+        life = life - 1
 
-    num_input = randint(0, max_progression)
-    print(num_input)
-    print(progression[num_input])
+        min_progression = 5
+        max_progression = randint(0, 10)
+        progres = (i[0:min_progression] + i[min_progression:max_progression])
 
-    progres_str = (" ".join(map(str, progression)))
-    num_replace = str(progres_str).replace(str(progression[num_input]), '..')
-    print(num_replace)
+        num_input = randint(0, max_progression)
+
+        progres_str = (" ".join(map(str, progres)))
+        num_replace = str(progres_str).replace(str(progres[num_input]), '..')
+        print(num_replace)
+
+        comparison_num_input = int(input('Your answer: '))
+        if comparison_num_input == progres[num_input]:
+            print('Correct!')
+        else:
+            print(f"'{comparison_num_input}'",
+                  "is wrong answer ;(. "
+                  "Correct answer was",
+                  f"'{progres[num_input]}'" + ". "
+                  "Let's try again,", name + "!")
+            break
+
+        if life <= 0:
+            print('Congratulations, ' + name + '!')
 
 
 result_progression()
