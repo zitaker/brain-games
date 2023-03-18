7b36c3c3f5f6c4bb4b57e6e1974ed191882dc2be
georg <georbearwolf.com>
2023-03-18 18:38:34 +0300

    [060] cheek 029

--- brain_games/scripts/games/brain_progression.py
+++ brain_games/scripts/games/brain_progression.py
@@ -1,5 +1,5 @@
 # !/usr/bin/env python3
-# import prompt
+import prompt
 from random import randint
 
 
@@ -13,59 +13,58 @@ def main():
 main()
 
 
-# def welcome_user_progression():
-#     names = prompt.string('May I have your name? ')
-#     print(f'{"Hello, " + names + "!"}')
-#     print('What number is missing in the progression?')
-#     return names
+def welcome_user_progression():
+    names = prompt.string('May I have your name? ')
+    print(f'{"Hello, " + names + "!"}')
+    print('What number is missing in the progression?')
+    return names
+
+
+name = welcome_user_progression()
+
+# def result_progression():
+#     # life - кол-во жизней
+#     n = 0
+#     life = 3
+#     for life in range(0, life + 1):
+#         n += life
+#         if life == 3:
+#             print('Congratulations ' + name + '!')
+#             break
+#
+#         def arithmetic_progression():
+#             progression_1 = randint(1, 10)
+#             progression_2 = randint(1, 10)
+#             progression_3 = randint(100, 150)
+#             i = (list(range(progression_2, progression_3, progression_1)))
+#             return i
+#
+#         i = arithmetic_progression()
+#
+#         min_progres = 6
+#         max_progres = randint(0, 10)
+#         #   progres - список
+#         progres = (i[0:min_progres]) + (i[min_progres:max_progres])
+#
+#         #   num_input - номер числа по индексу в рандомном списке
+#         num_input = randint(0, max_progres - 1)
+#
+#         points = '..'
+#         progres_str = (" ".join(map(str, progres)))
+#         num_replace = str(progres_str).replace(str(progres[num_input]), points)
+#         print(num_replace)
+#         print(progres[num_input])
+#
+#         comparison_num_input = int(input('Your answer: '))
+#         if comparison_num_input is progres[num_input]:
+#             print('Correct!')
+#         elif comparison_num_input is not progres[num_input]:
+#             print(f"'{comparison_num_input}'",
+#                   "is wrong answer ;(. "
+#                   "Correct answer was",
+#                   f"'{progres[num_input]}'" + ". "
+#                   "Let's try again,", name + "!")
+#             break
 #
 #
-# name = welcome_user_progression()
-name = '1234'
-
-def result_progression():
-    # life - кол-во жизней
-    n = 0
-    life = 3
-    for life in range(0, life + 1):
-        n += life
-        if life == 3:
-            print('Congratulations ' + name + '!')
-            break
-
-        def arithmetic_progression():
-            progression_1 = randint(1, 10)
-            progression_2 = randint(1, 10)
-            progression_3 = randint(100, 150)
-            i = (list(range(progression_2, progression_3, progression_1)))
-            return i
-
-        i = arithmetic_progression()
-
-        min_progres = 6
-        max_progres = randint(0, 10)
-        #   progres - список
-        progres = (i[0:min_progres]) + (i[min_progres:max_progres])
-
-        #   num_input - номер числа по индексу в рандомном списке
-        num_input = randint(0, max_progres - 1)
-
-        points = '..'
-        progres_str = (" ".join(map(str, progres)))
-        num_replace = str(progres_str).replace(str(progres[num_input]), points)
-        print(num_replace)
-        print(progres[num_input])
-
-        comparison_num_input = int(input('Your answer: '))
-        if comparison_num_input is progres[num_input]:
-            print('Correct!')
-        elif comparison_num_input is not progres[num_input]:
-            print(f"'{comparison_num_input}'",
-                  "is wrong answer ;(. "
-                  "Correct answer was",
-                  f"'{progres[num_input]}'" + ". "
-                  "Let's try again,", name + "!")
-            break
-
-
-result_progression()
+# result_progression()
