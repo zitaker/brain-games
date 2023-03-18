beebddf275a0936a64c238c67f20de0c43872320
georg <georbearwolf.com>
2023-03-18 18:30:19 +0300

    [059] cheek 028

--- brain_games/scripts/games/brain_progression.py
+++ brain_games/scripts/games/brain_progression.py
@@ -1,5 +1,5 @@
 # !/usr/bin/env python3
-import prompt
+# import prompt
 from random import randint
 
 
@@ -13,15 +13,15 @@ def main():
 main()
 
 
-def welcome_user_progression():
-    names = prompt.string('May I have your name? ')
-    print(f'{"Hello, " + names + "!"}')
-    print('What number is missing in the progression?')
-    return names
-
-
-name = welcome_user_progression()
-
+# def welcome_user_progression():
+#     names = prompt.string('May I have your name? ')
+#     print(f'{"Hello, " + names + "!"}')
+#     print('What number is missing in the progression?')
+#     return names
+#
+#
+# name = welcome_user_progression()
+name = '1234'
 
 def result_progression():
     # life - кол-во жизней
@@ -54,7 +54,7 @@ def result_progression():
         progres_str = (" ".join(map(str, progres)))
         num_replace = str(progres_str).replace(str(progres[num_input]), points)
         print(num_replace)
-        # print(progres[num_input])
+        print(progres[num_input])
 
         comparison_num_input = int(input('Your answer: '))
         if comparison_num_input is progres[num_input]:
