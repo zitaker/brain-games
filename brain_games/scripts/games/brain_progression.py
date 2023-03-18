75442821e34c37a55f33a1d4b3f00e55d5043428
georg <georbearwolf.com>
2023-03-18 19:11:28 +0300

    [063] cheek 032

--- brain_games/scripts/games/brain_progression.py
+++ brain_games/scripts/games/brain_progression.py
@@ -55,7 +55,7 @@ def result_progression():
         progres_str = (" ".join(map(str, progres)))
         num_replace = str(progres_str).replace(str(progres[num_input]), points)
         print(num_replace)
-        print(progres[num_input])
+        print(int(progres[num_input]))
 
         comparison_num_input = int(input('Your answer: '))
         if comparison_num_input is progres[num_input]:
