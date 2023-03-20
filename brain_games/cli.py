06c32b2e1cd48d2c800aae7272f9431ddb00bf65
georg <georbearwolf.com>
2023-03-20 15:26:36 +0300

    [072] cheek 041

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -5,9 +5,6 @@ def main():
     print('Welcome to the Brain Games!')
 
 
-main()
-
-
 def welcome_user():
     name = prompt.string('May I have your name? ')
     print(f'{"Hello, " + name + "!"}')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,15 +1,12 @@
 # !/usr/bin/env python3
 from brain_games.cli import welcome_user
 from brain_games.cli import main
-# from brain_games.scripts.games.brain_progression import main
-
-# def main():
-#     print('Welcome to the Brain Games!')
 
 
 if __name__ == '__main__':
     print('main')
-    main()
+
 
 print('brain-games')
+main()
 welcome_user()
